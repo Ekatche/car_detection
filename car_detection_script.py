d336fbb4761db7bcf8a40d465a53c7bbb483427a
elielK <atchokatche@gmail.com>
2022-05-06 16:36:02 +0200

    update du layout streamlit

--- car_detection_script.py
+++ car_detection_script.py
@@ -1,3 +1,17 @@
 import streamlit as st
 
-st.write('Hello this is a little projet about object detection on video')
+st.header('What is this project about ')
+
+st.write('Hello this is a little projet for our deep learning class')
+st.write('We were asked to design a model that is able to detect car in a video tape')
+
+st.sidebar.image("./image/logo.png")
+
+st.markdown('# Which model we will use ')
+
+st.markdown('## Why this model?  ')
+
+st.markdown('## The parameters  ')
+
+st.markdown('# Our data  ')
+
